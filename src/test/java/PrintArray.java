ff1a6c59108b2a8502d84b6e3165a4d73dfe2e27
ashwi <ashwi@LAPTOP-BCLHBJ3V>
2024-08-05 05:46:32 -0700

    First commit

---
+++ src/test/java/PrintArray.java
@@ -0,0 +1,19 @@
+
+public class PrintArray {
+
+	// write a program to find the numbers from the array who's addition is zero.ok??
+	public static void main(String[] args) {
+		int[] N = { -1, 1, 8, 3, -5, 5 };
+	
+			
+		for (int i = 0; i < N.length-1; i++) {
+			int sum = (N[i]+N[i+1]);
+			if (sum==0) {
+				System.out.println(N[i]+" "+N[i+1] + " = " + (N[i]+N[i+1]));
+			}
+			
+		}
+
+	}
+
+}
