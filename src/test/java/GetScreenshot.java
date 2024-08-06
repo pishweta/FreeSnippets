452a80c643eb6179874c5e6e54803e25046066e4
ashwi <ashwi@LAPTOP-BCLHBJ3V>
2024-08-06 02:17:26 -0700

    Added screenshot method.

---
+++ src/test/java/GetScreenshot.java
@@ -0,0 +1,9 @@
+
+public class GetScreenshot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Screenshot is succefully taken and added to the file you specified.");
+	}
+
+}
